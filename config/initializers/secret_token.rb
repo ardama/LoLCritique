73449e32b28a7bb67d9a49f1ff87157257b0ffe2
ardama <ardama@mit.edu>
2013-02-04 23:26:41 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoLCritique::Application.config.secret_token = '2e424090af2626783595f719cfd3f66ced1a1b81906b751450c94448fbf31fe3c1b3d3b172dfd74a9cd7fba98c69192ed3ce172d2f4803e2a121a372e0921fdd'
